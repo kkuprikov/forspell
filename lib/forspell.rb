3bf1f33bff9a31ed92a093f447da20ee3cb88b7e
Kirill Kuprikov <kkuprikov@gmail.com>
2018-11-26 18:26:15 +0300

    Bypassing class names check

--- lib/forspell.rb
+++ lib/forspell.rb
@@ -8,7 +8,27 @@ class Forspell
   end
 
   def check_spelling input
-    words = input.split(/[^[[:word:]]']+/)
-    @errors = words.select { |word| !dictionary.check?(word) }
+    words = input.split(/[^[[:word:]]_#]+/)
+    @errors = words.select{ |word| simple_word?(word) && !dictionary.check?(word) }
+  end
+
+  private
+
+  def simple_word? word
+    return false if seems_like_classname?(word)
+    /\w+/.match(word).to_s.size == word.size
+  end
+
+  def seems_like_classname? word
+    # prevents ClassName spellcheck
+    uppercased_chars_count(word) > 1
+  end
+
+  def uppercased_chars_count word
+    word.chars.select{ |char| uppercased?(char) }.size
+  end
+
+  def uppercased? character
+    /[[:upper:]]/.match(character)
   end
 end
--- spec/forspell_spec.rb
+++ spec/forspell_spec.rb
@@ -16,7 +16,8 @@ RSpec.describe Forspell do
     end
 
     it 'should skip class names and special words' do
-      checker.check_spelling 'See the examples in the Namespace::Examples, '
+      checker.check_spelling 'See examples in SomeNamespace::MyAwesomeExamples or AnotherAwesomeExamples, for instance: #ex4mple_load, darlin'
+      expect(checker.errors).to contain_exactly('darlin')
     end
   end
 end
