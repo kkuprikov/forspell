87631fd0e6a15a89c9344c2b083f06cba03dbf98
Kirill Kuprikov <kkuprikov@gmail.com>
2018-11-26 16:00:04 +0300

    Initial commit

---
+++ lib/forspell.rb
@@ -0,0 +1,18 @@
+require 'rubygems'
+require 'ffi/hunspell'
+
+class Forspell
+  attr_reader :errors, :dictionary
+
+  def initialize(dictionary_name: 'en_US')
+    @dictionary = FFI::Hunspell.dict(dictionary_name)
+  end
+
+  def check_spelling input
+    @errors = []
+    words = input.split(/[^[[:word:]]']+/)
+    words.map do |word|
+      @errors << word unless dictionary.check?(word)
+    end
+  end
+end
---
+++ spec/forspell_spec.rb
@@ -0,0 +1,16 @@
+require './lib/forspell'
+RSpec.describe Forspell do
+  let(:checker) { described_class.new } # with default en-Us dictionary
+
+  describe '#check_spelling' do
+    it 'should work if the spelling of each word is correct' do
+      checker.check_spelling('Richard Of York Gave Battle In Vain')
+      expect(checker.errors).to be_empty
+    end
+
+    it 'should return words with incorrect spelling' do
+      checker.check_spelling('s0me r4ndom stuff')
+      expect(checker.errors).to contain_exactly('s0me', 'r4ndom')
+    end
+  end
+end
