c3f0ce6ff8107056cc4b3565f408e18bd0075f86
Kirill Kuprikov <kkuprikov@gmail.com>
2018-12-03 19:25:27 +0300

    Review + minor fixes

--- lib/forspell.rb
+++ lib/forspell.rb
@@ -21,19 +21,23 @@ class Forspell
   def initialize(dictionary_name: 'en_US', **params)
     @format_class = params[:markup] ? RDOC_FORMATS[params[:markup]] : RDoc::RD
     fail "Unsupported markup format: #{ params[:markup] }" unless @format_class
+
     @dictionary = FFI::Hunspell.dict(dictionary_name)
     @logger = Logger.new(params[:logfile] || STDOUT)
   end
 
   def check_spelling input
     words = input.split(/[^[[:word:]]_#:]+/)
-    words.select{ |word| !dictionary.check?(word) }.sort.uniq
+    words.reject{ |word| dictionary.check?(word) }.sort.uniq
   end
 
   def check_docs docs
     hash_with_errors = docs.transform_values do |v| 
-      { docstring: v, errors: filter_code_objects(check_spelling(extract_text(v))) } 
-    end.select{ |k, v| !v[:errors].empty? }
+      { 
+        docstring: v, 
+        errors: filter_code_objects(check_spelling(extract_text(v))) 
+      } 
+    end.reject{ |k, v| v[:errors].empty? }
 
     pretty_print(hash_with_errors) unless hash_with_errors.empty?
   end
@@ -55,7 +59,7 @@ class Forspell
     end.to_h
 
     @logger.info 'Spellchecking result:'
-    @errors.each_pair { |object, errors| @logger.info "#{ object }, #{ errors.join(', ') } " }
+    @errors.each_pair { |object, errors| @logger.info "#{ object }, #{ errors.join(', ') }" }
   end
 
   def range_or_line_number range
@@ -66,7 +70,7 @@ class Forspell
     words.reject do |word|
       CODE_MARKERS.any? { |marker| word.chars.include?(marker) } ||
       word.count(('A'..'Z').to_a.join) > 1 ||
-      CUSTOM_DICTIONARY.include?(word)
+      CUSTOM_DICTIONARY.include?(word.downcase)
     end
   end
 end
